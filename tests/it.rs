b45773f13ae3ee5fa0d8952833b2f0ad5229b6ad
Francis De Brabandere <francisdb@gmail.com>
2024-05-18 10:02:39 +0200

    testing why test wont work on ci

--- tests/it.rs
+++ tests/it.rs
@@ -3,11 +3,6 @@
 // use indoc::indoc;
 // use pretty_assertions::assert_eq;
 
-/// It tries to parse all `.vbs` files going one level lower from the root of the project.
-/// We suggest to make sure you have https://github.com/jsm174/vpx-standalone-scripts cloned
-/// in the same directory as this project.
-///
-/// Run this test with `cargo test --release -- --nocapture --ignored try_parsing_all_vbs_files`
 #[test]
 fn try_formatting_all_vbs_files() {
     let paths = glob::glob("./testscripts/**/*.vbs")
